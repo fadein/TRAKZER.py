c2fdf907900d06d38c0842222301e5dee2a30c69
Erik at work <efalor@spillman.com>
2015-10-28 10:32:34 -0600

    now we're getting somewhere

--- TRAKZER.py
+++ TRAKZER.py
@@ -20,9 +20,19 @@ class HelpMeOutHere(object):
     def get_ip_address(self):
         return self.ip_address.hack_through_FBI_Firewall()
 
+    def lines(self, exe):
+        proc = subprocess.Popen(exe, shell=True, stdout=subprocess.PIPE)
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            yield line
+        
     def switch_to_Perl(self):
         print "Switching to Perl for the 90's lolz :P"
-        print subprocess.Popen("perl -e \"$|=1;for $_ ('Waiting', 'for', 'slow', 'Perl') { print $_.' '; sleep 1 } printf '%c' x 43,112,108,101,97,115,101,32,115,97,118,101,32,109,101,32,102,114,111,109,32,116,104,101,32,114,117,115,115,105,97,110,32,103,97,110,103,32,103,117,121,115,33,10\"", shell=True, stdout=subprocess.PIPE).stdout.read()
+        proc = self.lines("perl -e \"\$|=1;for ('Waiting', 'for', 'slow', 'Perl') { print qq{\$_\n}; sleep 1 } printf '%c' x 43,112,108,101,97,115,101,32,115,97,118,101,32,109,101,32,102,114,111,109,32,116,104,101,32,114,117,115,115,105,97,110,32,103,97,110,103,32,103,117,121,115,33,10\"")
+        for line in proc:
+            print line,
 
 if __name__ == "__main__":
     serious_business = HelpMeOutHere()
